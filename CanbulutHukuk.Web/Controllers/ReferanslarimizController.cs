acd8cd272f53c0e32427f74875fc48d050d1c061
02486164 <02486164@KSN80000487>
2024-02-27 12:30:00 +0300

    referanslar

---
+++ CanbulutHukuk.Web/Controllers/ReferanslarimizController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace CanbulutHukuk.Web.Controllers
+{
+    public class ReferanslarimizController : Controller
+    {
+        // GET: Referanslarimiz
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
