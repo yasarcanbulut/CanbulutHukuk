b24708a58f21f7525b8b729dee4328ce6d5ed1bd
02486164 <02486164@KSN80000487>
2024-02-27 16:04:20 +0300

    petapoco blogcontrollerdan kaldırıldı

--- CanbulutHukuk.Web/Controllers/BlogController.cs
+++ CanbulutHukuk.Web/Controllers/BlogController.cs
@@ -1,7 +1,4 @@
-﻿using CanbulutHukuk.Web.Models.CHModels;
-using CanbulutHukuk.Web.Models.ViewModels;
-using System.Linq;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CanbulutHukuk.Web.Controllers
 {
@@ -11,57 +8,6 @@ namespace CanbulutHukuk.Web.Controllers
         public ActionResult Index()
         {
             return View();
-        }
-
-        [HttpPost]
-        public ActionResult Index(string Search)
-        {
-            var dataContext = new PetaPoco.Database("sqlserverce");
-            BlogIndexVm vm = new BlogIndexVm()
-            {
-                BlogList = dataContext.Query<Article>("Select Article.*,Category.Name as CategoryName  from Article inner join Category on Category.Id = Article.CategoryId where Article.IsActive = 1 order by Article.ReleaseDate desc").ToList(),
-                LastBlogList = dataContext.Query<Article>("Select top 3 Article.*,Category.Name as CategoryName  from Article inner join Category on Category.Id = Article.CategoryId where Article.IsActive = 1 order by Article.ReleaseDate desc").ToList(),
-                CategoryList = dataContext.Query<Category>("Select * from Category where IsActive = 1 order by Name").ToList(),
-                TagList = dataContext.Query<Tags>("Select * from Tags where IsActive = 1 order by Name").ToList()
-            };
-
-            //BlogIndexVm vm = new BlogIndexVm()
-            //{
-            //    BlogList = new List<Article>(),
-            //    LastBlogList = new List<Article>(),
-            //    CategoryList = new List<Category>(),
-            //    TagList = new List<Tags>()
-            //};
-
-            return View(vm);
-        }
-
-        public ActionResult Sayfa(int Id)
-        {
-            var dataContext = new PetaPoco.Database("sqlserverce");
-
-            Article dbArticle = dataContext.Query<Article>("select Article.*,Category.Name as CategoryName from Article inner join Category on Category.Id = Article.CategoryId where Article.Id = @0", Id).FirstOrDefault();
-            dbArticle.ReadCount += 1;
-
-            dataContext.Save(dbArticle);
-
-            BlogSayfaVM vm = new BlogSayfaVM()
-            {
-                LastBlogList = dataContext.Query<Article>("Select top 3 Article.*,Category.Name as CategoryName  from Article inner join Category on Category.Id = Article.CategoryId where Article.IsActive = 1 order by Article.ReleaseDate desc").ToList(),
-                Post = dbArticle,
-                CategoryList = dataContext.Query<Category>("Select * from Category where IsActive = 1 order by Name").ToList(),
-                TagList = dataContext.Query<Tags>("Select * from Tags where IsActive = 1 order by Name").ToList()
-            };
-
-            //BlogSayfaVM vm = new BlogSayfaVM()
-            //{
-            //    LastBlogList = new List<Article>(),
-            //    Post = new Article(),
-            //    CategoryList = new List<Category>(),
-            //    TagList = new List<Tags>()
-            //};
-
-            return View(vm);
         }
 
         public ActionResult covid19_salgini_karsisinda_isci_isveren_iliskisi()
